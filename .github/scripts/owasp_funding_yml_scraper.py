310ee958cbe7fd71d772fa22c89b9550e28c4747
DonnieBLT <128622481+DonnieBLT@users.noreply.github.com>
2024-06-04 20:08:09 -0400

    Update owasp_funding_yml_scraper.py

--- .github/scripts/owasp_funding_yml_scraper.py
+++ .github/scripts/owasp_funding_yml_scraper.py
@@ -1,8 +1,6 @@
 import json
-import re
 import os
 import requests
-from bs4 import BeautifulSoup
 
 GITHUB_API_URL = "https://api.github.com"
 ORG_NAME = "OWASP"
@@ -16,19 +14,6 @@ def get_owasp_repos():
         repos.extend(response.json())
         url = response.links.get('next', {}).get('url')
     return repos
-
-def extract_github_links(url, project_name):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = soup.find_all('a', href=True)
-    
-    github_links = set()
-    for link in links:
-        match = re.match(r'https://github\.com/[^/]+/[^/#]+', link['href'].lower())
-        if match and project_name.lower() not in link['href'].lower():
-            github_links.add(match.group(0))
-    
-    return list(github_links)
 
 def check_funding_file(repo_name):
     funding_url = f'https://raw.githubusercontent.com/{ORG_NAME}/{repo_name}/master/.github/FUNDING.yml'
@@ -58,18 +43,14 @@ for project in data:
         continue
 
     print("project name", project_name)
-    github_url = project['html_url']
-    
-    repo_links = extract_github_links(github_url, project_name)
-    for repo in repo_links:
-        repo_name = '/'.join(repo.split('/')[-2:])
-        funding_url = check_funding_file(repo_name)
-        if funding_url:
-            project_links.append({
-                'project_name': project_name,
-                'funding_url': funding_url
-            })
-            print(f"Added project: {project_name} with funding URL: {funding_url}")
+    repo_name = project_name
+    funding_url = check_funding_file(repo_name)
+    if funding_url:
+        project_links.append({
+            'project_name': project_name,
+            'funding_url': funding_url
+        })
+        print(f"Added project: {project_name} with funding URL: {funding_url}")
 
 output_file = 'project_repos_links.json'
 with open(output_file, 'w') as f:
